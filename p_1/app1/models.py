4982be558aec6ffba8d1e18d4026122aab875e0d
JiangCheng817 <947093361@qq.com>
2019-02-13 13:18:49 +0800

    init

---
+++ p_1/app1/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField(max_length=10)
+    price = models.TextField(max_length=10)
