2627888b31de7649f2357a3944ec66099c8fefdd
JiangCheng817 <947093361@qq.com>
2019-02-13 14:39:33 +0800

    add migrations

--- p_1/app1/models.py
+++ p_1/app1/models.py
@@ -6,3 +6,9 @@ class Book(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.TextField(max_length=10)
     price = models.TextField(max_length=10)
+
+
+# class person(models.Model):
+#     id = models.IntegerField(primary_key=True)
+#     name = models.TextField(max_length=10)
+#     price = models.TextField(max_length=10)
